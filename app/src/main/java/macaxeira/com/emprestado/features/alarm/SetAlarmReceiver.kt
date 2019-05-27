21ff1a50b8d131fc30a132c7fe754e0f8f1bd547
silvano <silvano.pereira@gmail.com>
2019-05-27 11:15:25 -0300

    Fixed a bug that was preventing to set notification after boot

--- app/src/main/java/macaxeira/com/emprestado/features/alarm/SetAlarmReceiver.kt
+++ app/src/main/java/macaxeira/com/emprestado/features/alarm/SetAlarmReceiver.kt
@@ -3,33 +3,59 @@ package macaxeira.com.emprestado.features.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import macaxeira.com.emprestado.R
 import macaxeira.com.emprestado.data.DataRepository
+import macaxeira.com.emprestado.data.entities.Item
 import macaxeira.com.emprestado.utils.Utils
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
 
 class SetAlarmReceiver : BroadcastReceiver(), KoinComponent {
 
+    companion object {
+        private const val NO_FILTER = 0
+    }
+
     private val repository: DataRepository by inject()
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
         if (intent.action == "android.intent.action.BOOT_COMPLETED") {
-            val items = repository.getItemsByFilter(0).blockingGet()
-
-            for (item in items) {
-                if (item.remember && !item.isReturned) {
-                    val text: String = if (item.isMine)
-                        context.getString(R.string.notification_return_lent, item.description, item.person?.name)
-                    else
-                        context.getString(R.string.notification_return_borrowed, item.description,
-                                item.person?.name)
-
-                    val date = Utils.fromStringToTime(item.returnDate)
-
-                    NotificationScheduler.setAlarm(context, item.id!!.toInt(), date, text)
+            val so: SingleObserver<List<Item>> = object :SingleObserver<List<Item>> {
+                override fun onSuccess(t: List<Item>) {
+                    setNotifications(t, context)
                 }
+
+                override fun onError(e: Throwable) {
+                    e.printStackTrace()
+                }
+
+                override fun onSubscribe(d: Disposable) {}
+            }
+
+            repository.getItemsByFilter(NO_FILTER)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(so)
+        }
+    }
+
+    private fun setNotifications(items: List<Item>, context: Context) {
+        for (item in items) {
+            if (item.remember && !item.isReturned) {
+                val text: String = if (item.isMine)
+                    context.getString(R.string.notification_return_lent, item.description, item.person?.name)
+                else
+                    context.getString(R.string.notification_return_borrowed, item.description,
+                            item.person?.name)
+
+                val date = Utils.fromStringToTime(item.returnDate)
+
+                NotificationScheduler.setAlarm(context, item.id!!.toInt(), date, text)
             }
         }
     }
