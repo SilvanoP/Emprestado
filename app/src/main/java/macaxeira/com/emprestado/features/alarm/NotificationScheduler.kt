e2f85af1bec0b9cc431018f2cd95d36d497b9e17
silvano <silvano.pereira@gmail.com>
2019-08-09 21:05:04 -0300

    Adjusted icon position on Main Activity

--- app/src/main/java/macaxeira/com/emprestado/features/alarm/NotificationScheduler.kt
+++ app/src/main/java/macaxeira/com/emprestado/features/alarm/NotificationScheduler.kt
@@ -5,7 +5,6 @@ import android.app.Notification
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.TaskStackBuilder
